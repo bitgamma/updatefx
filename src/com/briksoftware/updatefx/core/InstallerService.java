da564d0fcea44123c0427934edce49658fb60b76
Michele Balistreri <michele@briksoftware.com>
2014-09-15 18:30:29 +0200

    added license info

--- src/com/briksoftware/updatefx/core/InstallerService.java
+++ src/com/briksoftware/updatefx/core/InstallerService.java
@@ -1,20 +1,85 @@
+/*
+ * Copyright (C) 2014 Michele Balistreri
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
 package com.briksoftware.updatefx.core;
 
-import java.io.File;
+import java.nio.file.Path;
 
+import javafx.application.Platform;
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
 
 public class InstallerService extends Service<Void> {
-
-	public InstallerService(File fileToInstall) {
-		// TODO Auto-generated constructor stub
+	private Path installer;
+	public InstallerService(Path installer) {
+		this.installer = installer;
 	}
 
 	@Override
 	protected Task<Void> createTask() {
-		// TODO Auto-generated method stub
-		return null;
+		return new Task<Void>() {
+			@Override
+			protected Void call() throws Exception {
+				String[] nameComponents = installer.getFileName().toString().split("\\.");
+				
+				if (nameComponents.length < 2) {
+					throw new IllegalArgumentException("Files without extensions are not supported yet");
+				}
+				
+				String extension = nameComponents[nameComponents.length - 1].toLowerCase();
+				
+				switch(extension) {
+				case "dmg":
+					handleDMGInstallation();
+					break;
+				case "exe":
+					handleEXEInstallation();
+					break;
+				case "msi":
+					handleMSIInstallation();
+					break;
+				default:
+					throw new IllegalArgumentException(String.format("installers with extension %s are not supported", extension));
+				}
+				
+				Platform.exit();
+				return null;
+			}
+		};
 	}
 
+	private void handleDMGInstallation() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private void handleEXEInstallation() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private void handleMSIInstallation() {
+		// TODO Auto-generated method stub
+		
+	}
 }
--- src/com/briksoftware/updatefx/util/package-info.java
+++ src/com/briksoftware/updatefx/util/package-info.java
@@ -21,42 +21,4 @@
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
  */
-package com.briksoftware.updatefx.core;
-
-import java.net.URL;
-
-import javafx.concurrent.Service;
-import javafx.concurrent.Task;
-
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.Unmarshaller;
-
-import com.briksoftware.updatefx.model.Application;
-import com.briksoftware.updatefx.model.Release;
-
-public class XMLRetrieverService extends Service<Application> {
-	private URL xmlURL;
-	
-	public XMLRetrieverService(URL xmlURL) {
-		this.xmlURL = xmlURL;
-	}
-
-	@Override
-	protected Task<Application> createTask() {
-		return new Task<Application>() {
-
-			@Override
-			protected Application call() throws Exception {
-				JAXBContext ctx = JAXBContext.newInstance(Application.class);
-				Unmarshaller um = ctx.createUnmarshaller();
-				Application app = (Application) um.unmarshal(xmlURL);
-				
-				for (Release release : app.getReleases()) {
-					release.setApplication(app);
-				}
-				
-				return app;
-			}
-		};
-	}
-}
+package com.briksoftware.updatefx.util;
