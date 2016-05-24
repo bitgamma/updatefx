c437a0a6da4eaab09254ebaa35c2275997bd5e93
mikecroall <mikebcroall@gmail.com>
2016-05-24 22:34:55 +0100

    Is platform now accepts any platform is platform wanted is Platform.independent

--- src/main/java/com/briksoftware/updatefx/core/UpdateDownloadService.java
+++ src/main/java/com/briksoftware/updatefx/core/UpdateDownloadService.java
@@ -40,89 +40,93 @@ import com.briksoftware.updatefx.model.Platform;
 import com.briksoftware.updatefx.model.Release;
 
 public class UpdateDownloadService extends Service<Path> {
-	private Release release;
+    private Release release;
 
-	public UpdateDownloadService(Release release) {
-		this.release = release;
-	}
+    public UpdateDownloadService(Release release) {
+        this.release = release;
+    }
 
-	@Override
-	protected Task<Path> createTask() {
-		return new Task<Path>() {
-			@Override
-			protected Path call() throws Exception {
-				Binary toDownload = null;
+    @Override
+    protected Task<Path> createTask() {
+        return new Task<Path>() {
+            @Override
+            protected Path call() throws Exception {
+                Binary toDownload = null;
 
-				for (Binary binary : release.getBinaries()) {
-					if (isCurrentPlatform(binary.getPlatform()) || binary.getPlatform() == Platform.independent) {
-						toDownload = binary;
-						break;
-					}
-				}
+                for (Binary binary : release.getBinaries()) {
+                    if (isCurrentPlatform(binary.getPlatform()) || binary.getPlatform() == Platform.independent) {
+                        toDownload = binary;
+                        break;
+                    }
+                }
 
-				if (toDownload == null) {
-					throw new IllegalArgumentException("This release does not contain binaries for this platform");
-				}
+                if (toDownload == null) {
+                    throw new IllegalArgumentException("This release does not contain binaries for this platform");
+                }
 
-				URL fileURL = toDownload.getHref();
-				URLConnection connection = fileURL.openConnection();
-				connection.connect();
+                URL fileURL = toDownload.getHref();
+                URLConnection connection = fileURL.openConnection();
+                connection.connect();
 
-				long len = connection.getContentLengthLong();
+                long len = connection.getContentLengthLong();
 
-				if (len == -1) {
-					len = toDownload.getSize();
-				}
+                if (len == -1) {
+                    len = toDownload.getSize();
+                }
 
-				updateProgress(0, len);
+                updateProgress(0, len);
 
-				String fileName = connection.getHeaderField("Content-Disposition");
-				if (fileName != null && fileName.contains("=")) {
-					fileName = fileName.split("=")[1];
-				} else {
-					String url = toDownload.getHref().getPath();
-					int lastSlashIdx = url.lastIndexOf('/');
-					
-					if (lastSlashIdx >= 0) {						
-						fileName = url.substring(lastSlashIdx + 1, url.length());
-					} else {
-						fileName = url;
-					}
-				}
+                String fileName = connection.getHeaderField("Content-Disposition");
+                if (fileName != null && fileName.contains("=")) {
+                    fileName = fileName.split("=")[1];
+                } else {
+                    String url = toDownload.getHref().getPath();
+                    int lastSlashIdx = url.lastIndexOf('/');
 
-				Path downloadFile = Paths.get(System.getProperty("java.io.tmpdir"), fileName);
-				
-				try(OutputStream fos = Files.newOutputStream(downloadFile, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.WRITE); 
-						BufferedInputStream is = new BufferedInputStream(connection.getInputStream())) {
-					byte[] buffer = new byte[512];
-					int n;
-					long totalDownload = 0;
-					
-					while ((n = is.read(buffer)) != -1) {
-						fos.write(buffer, 0, n);
-						totalDownload += n;
-						updateProgress(totalDownload, len);
-					}
-				}
-				
-				return downloadFile;
-			}
-		};
-	}
+                    if (lastSlashIdx >= 0) {
+                        fileName = url.substring(lastSlashIdx + 1, url.length());
+                    } else {
+                        fileName = url;
+                    }
+                }
 
-	private boolean isCurrentPlatform(Platform platform) {
-		String currentPlatform = System.getProperty("os.name").toLowerCase();
+                Path downloadFile = Paths.get(System.getProperty("java.io.tmpdir"), fileName);
 
-		if (currentPlatform.startsWith("mac")) {
-			return platform == Platform.mac;
-		} else if (currentPlatform.startsWith("windows")) {
-			if (System.getProperty("os.arch").contains("64")) {
-				return platform == Platform.win_x64;
-			} else {
-				return platform == Platform.win_x86;
-			}
-		} else {
-			throw new IllegalStateException("UpdateFX does not support this platform");
-		}
-	}
+                try (OutputStream fos = Files.newOutputStream(downloadFile, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.WRITE);
+                     BufferedInputStream is = new BufferedInputStream(connection.getInputStream())) {
+                    byte[] buffer = new byte[512];
+                    int n;
+                    long totalDownload = 0;
+
+                    while ((n = is.read(buffer)) != -1) {
+                        fos.write(buffer, 0, n);
+                        totalDownload += n;
+                        updateProgress(totalDownload, len);
+                    }
+                }
+
+                return downloadFile;
+            }
+        };
+    }
+
+    private boolean isCurrentPlatform(Platform platform) {
+        if (platform == Platform.independent) {
+            return true;
+        }
+
+        String currentPlatform = System.getProperty("os.name").toLowerCase();
+
+        if (currentPlatform.startsWith("mac")) {
+            return platform == Platform.mac;
+        } else if (currentPlatform.startsWith("windows")) {
+            if (System.getProperty("os.arch").contains("64")) {
+                return platform == Platform.win_x64;
+            } else {
+                return platform == Platform.win_x86;
+            }
+        } else {
+            throw new IllegalStateException("UpdateFX does not support this platform unless application is platform independent");
+        }
+    }
 }
